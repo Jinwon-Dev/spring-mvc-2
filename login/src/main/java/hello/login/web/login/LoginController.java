8a19a333bfc8cdf52e0b00d6fa6254adea3a019a
Jinwon-Dev <yjw9424@naver.com>
2023-10-17 00:17:42 +0900

    feat: 서블릿 HTTP 세션을 이용한 로그인 기능

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,6 +2,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -46,7 +48,7 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // TODO: 로그인 성공 처리
+        // 로그인 성공 처리
 
         // 쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
         Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
@@ -55,7 +57,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV2(@Valid
                           @ModelAttribute LoginForm form,
                           BindingResult bindingResult,
@@ -72,10 +74,37 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // TODO: 로그인 성공 처리
+        // 로그인 성공 처리
 
         // 세션 관리자를 통해 세션을 생성하고, 회원 데이터 보관
         sessionManager.createSession(loginMember, response);
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV3(@Valid
+                          @ModelAttribute LoginForm form,
+                          BindingResult bindingResult,
+                          HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+        // 세션이 있다면 있는 세션을 반환하고, 없다면 신규 세션을 생성
+        HttpSession session = request.getSession();
+
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
 
         return "redirect:/";
     }
@@ -86,9 +115,21 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+//    @PostMapping("/logout")
     public String logoutV2(HttpServletRequest request) {
         sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+
         return "redirect:/";
     }
 
